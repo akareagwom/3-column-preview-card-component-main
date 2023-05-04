aef00eb84f6f7d8346746dd39c3b785001886c39
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-05-04 14:58:32 +0100

    mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'bright-orange':"hsl(31, 77%, 52%)",
+        'Dark-cyan' : "hsl(184, 100%, 22%)",
+        'Very-dark-cyan': "hsl(179, 100%, 13%)"
+      }
+    },
   },
   plugins: [],
 }
